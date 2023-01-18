79aa0e79411af943939e07489ab3f5f95edf0b1f
egorw <egorw@message.com>
2023-01-19 01:08:32 +0300

    Добавьте файлы проекта.

---
+++ src/Controls/TreeListView.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OkoloIt.Wpf.Controls
+{
+    /// <summary>
+    /// Выполните шаги 1a или 1b, а затем 2, чтобы использовать этот пользовательский элемент управления в файле XAML.
+    ///
+    /// Шаг 1a. Использование пользовательского элемента управления в файле XAML, существующем в текущем проекте.
+    /// Добавьте атрибут XmlNamespace в корневой элемент файла разметки, где он
+    /// будет использоваться:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:OkoloIt.Wpf.Controls"
+    ///
+    ///
+    /// Шаг 1б. Использование пользовательского элемента управления в файле XAML, существующем в другом проекте.
+    /// Добавьте атрибут XmlNamespace в корневой элемент файла разметки, где он
+    /// будет использоваться:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:OkoloIt.Wpf.Controls;assembly=OkoloIt.Wpf.Controls"
+    ///
+    /// Потребуется также добавить ссылку из проекта, в котором находится файл XAML,
+    /// на данный проект и пересобрать во избежание ошибок компиляции:
+    ///
+    ///     Щелкните правой кнопкой мыши нужный проект в обозревателе решений и выберите
+    ///     "Добавить ссылку"->"Проекты"->[Поиск и выбор проекта]
+    ///
+    ///
+    /// Шаг 2)
+    /// Теперь можно использовать элемент управления в файле XAML.
+    ///
+    ///     <MyNamespace:TreeListView/>
+    ///
+    /// </summary>
+    public class TreeListView : Control
+    {
+        static TreeListView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TreeListView), new FrameworkPropertyMetadata(typeof(TreeListView)));
+        }
+    }
+}
