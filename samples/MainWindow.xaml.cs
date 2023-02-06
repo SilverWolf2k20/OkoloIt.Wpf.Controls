86977f95caf25f76155d4d944ae20fb0fb1d4982
eafilatov <eafilatov@OASU_33K_323_3>
2023-02-06 16:16:46 +0300

    feat: реализован элемент "Поле для ввода пароля"

--- samples/MainWindow.xaml.cs
+++ samples/MainWindow.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security;
 using System.Windows;
+using System.Windows.Input;
 
 namespace OkoloIt.Wpf.Controls.Samples
 {
@@ -12,14 +17,12 @@ namespace OkoloIt.Wpf.Controls.Samples
         {
             InitializeComponent();
 
-            tlistProducts.ItemsSource = new List<Node>() {
+            /*tlistProducts.ItemsSource = new List<Node>() {
                 new Node() { IsExpandable = true, Data = new User("Ivan 228", "14"), Level = 1 },
                 new Node() { IsExpandable = true, Data = new User("Ivan 337", "16"), Level = 2 },
                 new Node() { IsExpandable = true, Data = new User("Ivan 322", "15"), Level = 1 },
                 new Node() { IsExpandable = true, Data = new User("Ivan 123", "11"), Level = 2 },
-            };
-
-            UpdateLayout();
+            };*/
         }
 
         public class User
@@ -33,5 +36,95 @@ namespace OkoloIt.Wpf.Controls.Samples
             public string Name { get; set; }
             public string Age { get; set; }
         }
+
     }
+
+    public class Model : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private SecureString _password;
+
+        public Model()
+        {
+            _password = new SecureString();
+            Click = new Command(OnClick);
+        }
+
+        public SecureString Password {
+            get => _password;
+            set {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        private void OnClick(object? sender)
+        {
+            Trace.WriteLine($"[PBOX]{ConvertToString(Password)}");
+
+            var password = new SecureString();
+
+            password.AppendChar('0');
+            password.AppendChar('1');
+            password.AppendChar('2');
+            password.AppendChar('3');
+
+            Password = password;
+        }
+
+        public ICommand Click { get; init; }
+
+        public void OnPropertyChanged(string propertyName)
+           => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public static string ConvertToString(SecureString text)
+        {
+            IntPtr ptr = IntPtr.Zero;
+            string result = string.Empty;
+            try {
+                ptr = Marshal.SecureStringToGlobalAllocUnicode(text);
+                result = Marshal.PtrToStringUni(ptr)!;
+            }
+            finally {
+                if (ptr != IntPtr.Zero)
+                    Marshal.ZeroFreeGlobalAllocUnicode(ptr);
+            }
+            return result;
+        }
+    }
+
+    class Command : ICommand
+    {
+        private readonly Predicate<object?>? _canExecute;
+        private readonly Action<object?>? _execute;
+
+        public Command(Action<object?>? execute)
+            : this(execute, null)
+        {
+        }
+
+        public Command(Action<object?>? execute, Predicate<object?>? canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+
+        public bool CanExecute(object? parameter)
+            => _canExecute == null || _canExecute(parameter);
+
+        public void Execute(object? parameter)
+        {
+            if (_execute is null)
+                return;
+
+            _execute(parameter);
+        }
+    }
+
 }
---
+++ src/Controls/PasswordBox.cs
@@ -0,0 +1,153 @@
+﻿using System.Security;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace OkoloIt.Wpf.Controls
+{
+    /// <summary>
+    /// Поле для ввода пароля.
+    /// </summary>
+    public sealed class PasswordBox : TextBox
+    {
+        #region Public Fields
+
+        /// <summary>
+        /// Настройка пароля.
+        /// </summary>
+        public readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register(
+                nameof(Password),
+                typeof(SecureString),
+                typeof(PasswordBox),
+                new PropertyMetadata(new SecureString(), new PropertyChangedCallback(OnPasswordChange))
+            );
+
+        #endregion Public Fields
+
+        #region Public Constructors
+
+        /// <summary>
+        /// Создает экземпляр поля для ввода пароля.
+        /// </summary>
+        public PasswordBox()
+        {
+            PreviewTextInput += OnPreviewTextInput;
+            PreviewKeyDown += OnPreviewKeyDown;
+
+            CommandManager.AddPreviewExecutedHandler(this, PreviewExecutedHandler);
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        /// <summary>
+        /// Пароль.
+        /// </summary>
+        public SecureString Password {
+            get { return (SecureString)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+        #endregion Public Properties
+
+        #region Private Methods
+
+        private static void OnPasswordChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var passwordBox = (PasswordBox)d;
+            passwordBox.Text = new string('●', ((SecureString)e.NewValue).Length);
+        }
+
+        private void AddText(string text)
+        {
+            // Удаление выделенного участка.
+            if (SelectionLength > 0)
+                RemoveText(SelectionStart, SelectionLength);
+
+            int caretIndex = CaretIndex;
+
+            foreach (char c in text) {
+                // Обновление пароля.
+                SecureString password = Password.Copy();
+                password.InsertAt(caretIndex++, c);
+                Password = password;
+            }
+
+            CaretIndex = caretIndex;
+        }
+
+        private void OnPreviewKeyDown(object sender, KeyEventArgs args)
+        {
+            Key pressedKey = args.Key == Key.System ? args.SystemKey : args.Key;
+
+            if (pressedKey == Key.Enter) {
+                args.Handled = true;
+                return;
+            }
+
+            if (pressedKey == Key.Space) {
+                AddText(" ");
+                args.Handled = true;
+                return;
+            }
+
+            if ((pressedKey == Key.Delete || pressedKey == Key.Back) && SelectionLength > 0) {
+                RemoveText(SelectionStart, SelectionLength);
+                args.Handled = true;
+                return;
+            }
+
+            if (pressedKey == Key.Back && CaretIndex > 0) {
+                int caretIndex = CaretIndex;
+
+                if (CaretIndex > 0 && CaretIndex < Text.Length)
+                    --caretIndex;
+
+                RemoveText(CaretIndex - 1, 1);
+                CaretIndex = caretIndex;
+
+                args.Handled = true;
+                return;
+            }
+
+            if (pressedKey == Key.Delete && CaretIndex < Text.Length) {
+                RemoveText(CaretIndex, 1);
+                args.Handled = true;
+                return;
+            }
+        }
+
+        private void OnPreviewTextInput(object sender, TextCompositionEventArgs args)
+        {
+            AddText(args.Text);
+            args.Handled = true;
+        }
+
+        private void PreviewExecutedHandler(object sender, ExecutedRoutedEventArgs args)
+        {
+            if (args.Command == ApplicationCommands.Copy || args.Command == ApplicationCommands.Cut)
+                args.Handled = true;
+
+            if (args.Command == ApplicationCommands.Paste && Clipboard.ContainsText()) {
+                AddText(Clipboard.GetText());
+                args.Handled = true;
+            }
+        }
+        private void RemoveText(int startIndex, int count)
+        {
+            int caretIndex = CaretIndex;
+
+            for (int i = 0; i < count; ++i) {
+                SecureString password = Password.Copy();
+                password.RemoveAt(startIndex);
+                Password = password;
+            }
+
+            CaretIndex = caretIndex;
+        }
+
+        #endregion Private Methods
+    }
+}
