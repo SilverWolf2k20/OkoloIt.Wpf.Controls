00f1b7eff8f801f253ad85e215e5ac02bc2a9933
eafilatov <eafilatov@OASU_33K_323_3>
2023-02-06 14:12:10 +0300

    feat: добавлен элемент "Дерево"

--- src/Controls/TreeListView.cs
+++ src/Controls/TreeListView.cs
@@ -1,42 +1,93 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace OkoloIt.Wpf.Controls
 {
-    /// <summary>
-    /// Выполните шаги 1a или 1b, а затем 2, чтобы использовать этот пользовательский элемент управления в файле XAML.
-    ///
-    /// Шаг 1a. Использование пользовательского элемента управления в файле XAML, существующем в текущем проекте.
-    /// Добавьте атрибут XmlNamespace в корневой элемент файла разметки, где он
-    /// будет использоваться:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:OkoloIt.Wpf.Controls"
-    ///
-    ///
-    /// Шаг 1б. Использование пользовательского элемента управления в файле XAML, существующем в другом проекте.
-    /// Добавьте атрибут XmlNamespace в корневой элемент файла разметки, где он
-    /// будет использоваться:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:OkoloIt.Wpf.Controls;assembly=OkoloIt.Wpf.Controls"
-    ///
-    /// Потребуется также добавить ссылку из проекта, в котором находится файл XAML,
-    /// на данный проект и пересобрать во избежание ошибок компиляции:
-    ///
-    ///     Щелкните правой кнопкой мыши нужный проект в обозревателе решений и выберите
-    ///     "Добавить ссылку"->"Проекты"->[Поиск и выбор проекта]
-    ///
-    ///
-    /// Шаг 2)
-    /// Теперь можно использовать элемент управления в файле XAML.
-    ///
-    ///     <MyNamespace:TreeListView/>
-    ///
-    /// </summary>
-    public class TreeListView : Control
+    public class TreeListView : ListView
     {
+        #region Good
+
+        protected override DependencyObject GetContainerForItemOverride()
+            => new TreeListViewItem();
+
+        protected override bool IsItemItsOwnContainerOverride(object? item)
+            => item is TreeListViewItem;
+
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
+        {
+            if (element is not TreeListViewItem ti || item is not Node node)
+                return;
+
+            ti.Node = item as Node;
+            base.PrepareContainerForItemOverride(element, node.Data);
+        }
+
+        #endregion Good
+
         static TreeListView()
         {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(TreeListView), new FrameworkPropertyMetadata(typeof(TreeListView)));
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(TreeListView),
+                new FrameworkPropertyMetadata(typeof(TreeListView))
+            );
         }
+    }
+
+    public class TreeListViewItem : ListViewItem, INotifyPropertyChanged
+    {
+        private Node? _node;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public Node? Node {
+            get => _node;
+            internal set {
+                _node = value;
+                OnPropertyChanged(nameof(Node));
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public class Node
+    {
+        public bool IsExpandable { get; set; } = default;
+        public object Data { get; set; } = new object();
+        public int Level { get; set; } = default;
+    }
+
+    internal class CanExpandConverter : IValueConverter
+    {
+        public object Convert(object o, Type type, object parameter, CultureInfo culture)
+        {
+            if ((bool)o)
+                return Visibility.Visible;
+
+            return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object o, Type type, object parameter, CultureInfo culture)
+            => DependencyProperty.UnsetValue;
+    }
+
+    internal class LevelToIndentConverter : IValueConverter
+    {
+        private const double IndentSize = 19.0;
+
+        public object Convert(object o, Type type, object parameter, CultureInfo culture)
+        {
+            Trace.WriteLine($"########################## Level {(int)o * IndentSize}");
+            return new Thickness((int)o * IndentSize, 0, 0, 0);
+        }
+
+        public object ConvertBack(object o, Type type, object parameter, CultureInfo culture)
+            => DependencyProperty.UnsetValue;
     }
 }
