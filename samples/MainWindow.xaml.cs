9b8a2bb24ff7eb55856abe8635d7123f24f0160b
eafilatov <eafilatov@OASU_33K_323_3>
2023-02-06 14:11:29 +0300

    feat: добавлен проект с примерами работы библиотеки

--- samples/MainWindow.xaml.cs
+++ samples/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OkoloIt.Wpf.Controls.Samples
 {
@@ -23,6 +11,27 @@ namespace OkoloIt.Wpf.Controls.Samples
         public MainWindow()
         {
             InitializeComponent();
+
+            tlistProducts.ItemsSource = new List<Node>() {
+                new Node() { IsExpandable = true, Data = new User("Ivan 228", "14"), Level = 1 },
+                new Node() { IsExpandable = true, Data = new User("Ivan 337", "16"), Level = 2 },
+                new Node() { IsExpandable = true, Data = new User("Ivan 322", "15"), Level = 1 },
+                new Node() { IsExpandable = true, Data = new User("Ivan 123", "11"), Level = 2 },
+            };
+
+            UpdateLayout();
+        }
+
+        public class User
+        {
+            public User(string name, string age)
+            {
+                Name = name;
+                Age = age;
+            }
+
+            public string Name { get; set; }
+            public string Age { get; set; }
         }
     }
 }
