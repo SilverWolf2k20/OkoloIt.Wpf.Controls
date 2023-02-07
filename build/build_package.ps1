c811875f67690e7e63e6245599a7bdafec18246e
eafilatov <eafilatov@OASU_33K_323_3>
2023-02-07 18:47:04 +0300

    fix: исправлено определение пути

--- build/build_package.ps1
+++ build/build_package.ps1
@@ -1,4 +1,6 @@
 ﻿# Clear Release folder
+$sourceDirectory = "..\src";
+$csProjfileName = $sourceDirectory + "\OkoloIt.Wpf.Controls.csproj";
 $packagePath = $sourceDirectory + "\bin\Release\*.nupkg";
 
 Remove-Item -Path $packagePath;
