a90cb95e2e1093a34e5e5cfccb19f83bdd60162a
eafilatov <eafilatov@OASU_33K_323_3>
2023-03-09 21:48:15 +0300

    feat: добавлено построение сдвига ветвей дерева

--- samples/MainWindow.xaml.cs
+++ samples/MainWindow.xaml.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Globalization;
 using System.Runtime.InteropServices;
 using System.Security;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace OkoloIt.Wpf.Controls.Samples
 {
@@ -17,36 +23,76 @@ namespace OkoloIt.Wpf.Controls.Samples
         {
             InitializeComponent();
 
-            /*tlistProducts.ItemsSource = new List<Node>() {
-                new Node() { IsExpandable = true, Data = new User("Ivan 228", "14"), Level = 1 },
-                new Node() { IsExpandable = true, Data = new User("Ivan 337", "16"), Level = 2 },
-                new Node() { IsExpandable = true, Data = new User("Ivan 322", "15"), Level = 1 },
-                new Node() { IsExpandable = true, Data = new User("Ivan 123", "11"), Level = 2 },
-            };*/
+            tree.ItemsSource = new ObservableCollection<TreeListViewNode>() {
+                new TreeListViewNode() { Data = "123423-2354254-345235434", Level= 1 },
+                new TreeListViewNode() { Data = "223423-2354254-345235434", Level= 2 },
+                new TreeListViewNode() { Data = "323423-2354254-345235434", Level= 3 },
+                new TreeListViewNode() { Data = "423423-2354254-345235434", Level= 4 },
+            };
         }
     }
 
     public class User
     {
-        public User(string name, string age)
+        public User(string name, int age)
         {
             Name = name;
             Age = age;
         }
 
         public string Name { get; set; }
-        public string Age { get; set; }
+        public int Age { get; set; }
     }
+
+    /*public class Node
+    {
+        public User User { get; set; } = new User(string.Empty, default);
+        public bool IsRed { get; set; } = false;
+        public ObservableCollection<Node> Nodes { get; set; } = new();
+    }*/
 
     public class Model : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
+
         private SecureString _password;
 
         public Model()
         {
             _password = new SecureString();
             Click = new Command(OnClick);
+
+            /*_nodes = new() {
+                new Node
+                {
+                    User = new User("Вика", 19),
+                    Nodes = new ObservableCollection<Node>
+                    {
+                        new Node { User = new User("Катя", 7), IsRed = true },
+                        new Node { User = new User("Денис", 23) },
+                        new Node {
+                            User = new User("Яна", 30),
+                            Nodes = new ObservableCollection<Node> {
+                                new Node { User = new User("Виталик", 15) },
+                                new Node { User = new User("Антон", 31), IsRed = true },
+                                new Node { User = new User("Ира", 32), IsRed = true },
+                                new Node { User = new User("Аня", 18) },
+                            }
+                        }
+                    }
+                },
+                new Node {
+                    User = new User("Лера", 20),
+                    Nodes = new ObservableCollection<Node> {
+                        new Node { User = new User("Петя", 9) },
+                        new Node { User = new User("Вася", 24), IsRed = true },
+                        new Node { User = new User("Серега", 17) }
+                    }
+                },
+                new Node { User = new User("Коля", 15), IsRed = true },
+                new Node { User = new User("Кирилл", 11) },
+                new Node { User = new User("Иван", 10) }
+            };*/
         }
 
         public SecureString Password {
@@ -92,7 +138,35 @@ namespace OkoloIt.Wpf.Controls.Samples
         }
     }
 
-    class Command : ICommand
+    public class BoolToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isRed = (bool)value;
+
+            if (isRed)
+                return Brushes.Red;
+
+            return Brushes.Black;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => DependencyProperty.UnsetValue;
+    }
+
+    public class NodeToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var user = (User)value;
+            return $"{user.Name} - {user.Age}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => DependencyProperty.UnsetValue;
+    }
+
+    public class Command : ICommand
     {
         private readonly Predicate<object?>? _canExecute;
         private readonly Action<object?>? _execute;
--- src/Controls/TreeListView.cs
+++ src/Controls/TreeListView.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Globalization;
+﻿using System.Globalization;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.ComponentModel;
 
 namespace OkoloIt.Wpf.Controls
 {
     public class TreeListView : ListView
     {
-        #region Good
-
         protected override DependencyObject GetContainerForItemOverride()
             => new TreeListViewItem();
 
@@ -20,31 +17,42 @@ namespace OkoloIt.Wpf.Controls
 
         protected override void PrepareContainerForItemOverride(DependencyObject element, object item)
         {
-            if (element is not TreeListViewItem ti || item is not Node node)
+            if (element is not TreeListViewItem ti || item is not TreeListViewNode node)
                 return;
 
-            ti.Node = item as Node;
-            base.PrepareContainerForItemOverride(element, node.Data);
+            ti.Node = item as TreeListViewNode;
+            base.PrepareContainerForItemOverride(element, node);
         }
-
-        #endregion Good
 
         static TreeListView()
         {
             DefaultStyleKeyProperty.OverrideMetadata(
-                typeof(TreeListView),
+                typeof(TreeListView), 
                 new FrameworkPropertyMetadata(typeof(TreeListView))
             );
         }
     }
 
+    public class LevelToIndentConverter : IValueConverter
+    {
+        private const double IndentSize = 19.0;
+
+        public object Convert(object o, Type type, object parameter, CultureInfo culture)
+        {
+            return new Thickness((int)o * IndentSize, 0, 0, 0);
+        }
+
+        public object ConvertBack(object o, Type type, object parameter, CultureInfo culture)
+            => DependencyProperty.UnsetValue;
+    }
+
     public class TreeListViewItem : ListViewItem, INotifyPropertyChanged
     {
-        private Node? _node;
+        private TreeListViewNode? _node;
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        public Node? Node {
+        public TreeListViewNode? Node {
             get => _node;
             internal set {
                 _node = value;
@@ -56,38 +64,9 @@ namespace OkoloIt.Wpf.Controls
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 
-    public class Node
+    public class TreeListViewNode
     {
-        public bool IsExpandable { get; set; } = default;
         public object Data { get; set; } = new object();
         public int Level { get; set; } = default;
-    }
-
-    internal class CanExpandConverter : IValueConverter
-    {
-        public object Convert(object o, Type type, object parameter, CultureInfo culture)
-        {
-            if ((bool)o)
-                return Visibility.Visible;
-
-            return Visibility.Hidden;
-        }
-
-        public object ConvertBack(object o, Type type, object parameter, CultureInfo culture)
-            => DependencyProperty.UnsetValue;
-    }
-
-    internal class LevelToIndentConverter : IValueConverter
-    {
-        private const double IndentSize = 19.0;
-
-        public object Convert(object o, Type type, object parameter, CultureInfo culture)
-        {
-            Trace.WriteLine($"########################## Level {(int)o * IndentSize}");
-            return new Thickness((int)o * IndentSize, 0, 0, 0);
-        }
-
-        public object ConvertBack(object o, Type type, object parameter, CultureInfo culture)
-            => DependencyProperty.UnsetValue;
     }
 }
